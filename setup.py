5934486c6c43125f6dd9bc580371aabf0fe680e7
David Onder <donder@haywood.edu>
2022-03-25 15:30:42 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
 setup(name='pycolleague',
-      version='0.1',
+      version='0.1.1',
       description="A module for accessing data exported from Colleague SIS. Includes the ability to work with data exported as CSVs or data imported into the SAS Data Mart or CCDW database",
       url='https://www.haywood.edu/',
       author='David Onder',
       author_email='donder@haywood.edu',
       license='GNU GENERAL PUBLIC LICENSE v3.0',
       packages=['pycolleague'],
+      install_requires=[
+            'sqlalchemy',
+            'pyodbc',
+            'pyyaml'
+      ],
       zip_safe=False)
