17d5b6c368c441f782f9a978ab38fa54b33addfb
David Onder <donder@haywood.edu>
2022-05-27 17:43:34 -0400

    Upped version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycolleague',
-      version='0.1.1',
+      version='0.1.5',
       description="A module for accessing data exported from Colleague SIS. Includes the ability to work with data exported as CSVs or data imported into the SAS Data Mart or CCDW database",
       url='https://www.haywood.edu/',
       author='David Onder',
