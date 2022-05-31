83bf95768e5b5e5cf7a4080aa3cc5a5002ec7a38
David Onder <donder@haywood.edu>
2022-05-31 14:50:27 -0400

    Updated project setup files.

---
+++ setup-SAV.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setup(
+    # name="pycolleague",
+    # version="0.1.5",
+    # description="A module for accessing data exported from Colleague SIS. Includes the ability to work with data exported as CSVs or data imported into the SAS Data Mart or CCDW database",
+    #     long_description=long_description,
+    #     long_description_content_type="text/markdown",
+    #     url="https://www.haywood.edu/",
+    #     author="David Onder",
+    #     author_email="donder@haywood.edu",
+    #     license="GNU GENERAL PUBLIC LICENSE v3.0",
+    #     packages=find_packages(),
+    #     install_requires=[
+    #         "sqlalchemy",
+    #         "pyodbc",
+    #         "pyyaml",
+    #         "python-dotenv",
+    #         "pandas",
+    #         "pydantic",
+    #     ],
+    # zip_safe=False,
+)
