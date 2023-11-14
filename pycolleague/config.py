06d1574add66839019274afdef0943a1bad45cb3
David Onder <donder@haywood.edu>
2023-11-14 16:40:56 -0500

    Upgraded pydantic to 2.5.0.

--- pycolleague/config.py
+++ pycolleague/config.py
@@ -1,49 +1,92 @@
 import os
 from os.path import exists
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 import yaml
 from dotenv import dotenv_values
-from pydantic import BaseModel, BaseSettings, Field
-from pydantic.env_settings import SettingsSourceCallable
+from pydantic import BaseModel, Field
+from pydantic.fields import FieldInfo
+from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource
 
 
-def yml_config_setting(settings: BaseSettings) -> dict[str, Any]:
-    config = {
-        "HAYWOODCC_CFG_FULL_PATH": "",
-        "HAYWOODCC_CFG_PATH": ".",
-        "HAYWOODCC_CFG_FN": "config.yml",
-        **os.environ,
-        **dotenv_values("HAYWOODCC_CFG_FULL_PATH"),
-        **dotenv_values("HAYWOODCC_CFG_PATH"),
-        **dotenv_values("HAYWOODCC_CFG_FN"),
-    }
+class yml_config_setting(PydanticBaseSettingsSource):
 
-    if config["HAYWOODCC_CFG_FULL_PATH"] != "":
-        config_file = config["HAYWOODCC_CFG_FULL_PATH"]
-    elif config["HAYWOODCC_CFG_FN"] != "":
-        if config["HAYWOODCC_CFG_PATH"] != "":
-            config_file = os.path.join(
-                config["HAYWOODCC_CFG_PATH"], config["HAYWOODCC_CFG_FN"]
+    __config: Dict[str, Any] = {}
+
+    env_prefix: str = "CCDW_CFG"
+
+    def load_yml_file(self) -> Dict[str, Any]:
+        used_env = "<None Found>"
+
+        if (self.__config == {}):
+            config = {
+                f"{self.env_prefix}_FULL_PATH": "",
+                f"{self.env_prefix}_CFG_PATH": ".",
+                f"{self.env_prefix}_CFG_FN": "config.yml",
+                **os.environ,
+                **dotenv_values(f"{self.env_prefix}_FULL_PATH"),
+                **dotenv_values(f"{self.env_prefix}_PATH"),
+                **dotenv_values(f"{self.env_prefix}_FN"),
+            }
+
+            if f"{self.env_prefix}_FULL_PATH" in config and config[f"{self.env_prefix}_FULL_PATH"] != "":
+                config_file = config[f"{self.env_prefix}_FULL_PATH"]
+                used_env = f"{self.env_prefix}_FULL_PATH"
+            elif f"{self.env_prefix}_FN" in config and config[f"{self.env_prefix}_FN"] != "":
+                if f"{self.env_prefix}_PATH" in config and config[f"{self.env_prefix}_PATH"] != "":
+                    used_env = f"{self.env_prefix}_PATH and {self.env_prefix}_FN"
+                    config_file = os.path.join(
+                        config[f"{self.env_prefix}_PATH"], config[f"{self.env_prefix}_FN"]
+                    )
+                else:
+                    used_env = f"{self.env_prefix}_FN"
+                    config_file = os.path.join(".", config[f"{self.env_prefix}_FN"])
+            else:
+                config_file = os.path.join(".", "config.yml")
+
+            if exists(config_file):
+                with open(config_file, "r") as f:
+                    config_dict = yaml.safe_load(f)
+                config_dict["config"]["location"] = config_file
+            else:
+                config_dict = {}
+                raise Exception(f"The configuration file [{config_file}] does not exist. Used environment variable [{used_env}].")
+
+            __config = config_dict
+
+        return __config
+
+    def get_field_value(
+        self, field: FieldInfo, field_name: str
+    ) -> Tuple[Any, str, bool]:
+        config_dict = self.load_yml_file()
+        field_value = config_dict[field_name]
+        return field_value, field_name, False
+
+    def prepare_field_value(
+        self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
+    ) -> Any:
+        return value
+
+    def __call__(self) -> Dict[str, Any]:
+        d: Dict[str, Any] = {}
+
+        for field_name, field in self.settings_cls.model_fields.items():
+            field_value, field_key, value_is_complex = self.get_field_value(
+                field, field_name
             )
-        else:
-            config_file = os.path.join(".", config["HAYWOODCC_CFG_FN"])
-    else:
-        config_file = os.path.join(".", "config.yml")
+            field_value = self.prepare_field_value(
+                field_name, field, field_value, value_is_complex
+            )
+            if field_value is not None:
+                d[field_key] = field_value
 
-    if exists(config_file):
-        with open(config_file, "r") as f:
-            config_dict = yaml.safe_load(f)
-        config_dict["config"]["location"] = config_file
-    else:
-        config_dict = {}
-    return config_dict
-
+        return d
 
 class SchoolModel(BaseModel):
     name: Optional[str] = ""
     abbrev: Optional[str] = ""
-    ipeds: Optional[str] = ""
+    ipeds: Optional[Union[str,int]] = ""
     fice: Optional[str] = ""
     usgov: Optional[int] = None
     ncccs: Optional[int] = None
@@ -89,7 +132,7 @@ class Informer(BaseModel):
     export_path_meta: Optional[str] = ""
     stage_path: Optional[str] = ""
     prefix: Optional[str] = ""
-    latest_version: Optional[str] = ""
+    latest_version: Optional[Union[str,int]] = ""
 
 
 class CCDWModel(BaseModel):
@@ -107,13 +150,13 @@ class CCDWModel(BaseModel):
 
 
 class StatusFieldsModel(BaseModel):
-    ACAD_PROGRAMS: Optional[List[str]] = ""
-    APPLICATIONS: Optional[List[str]] = ""
-    COURSES: Optional[List[str]] = ""
-    STUDENT_ACAD_CRED: Optional[List[str]] = ""
-    STUDENT_PROGRAMS: Optional[List[str]] = ""
-    STUDENT_TERMS: Optional[List[str]] = ""
-    XCC_ACAD_PROGRAM_REQMTS: Optional[List[str]] = ""
+    ACAD_PROGRAMS: Optional[Union[List[str],str]] = ""
+    APPLICATIONS: Optional[Union[List[str],str]] = ""
+    COURSES: Optional[Union[List[str],str]] = ""
+    STUDENT_ACAD_CRED: Optional[Union[List[str],str]] = ""
+    STUDENT_PROGRAMS: Optional[Union[List[str],str]] = ""
+    STUDENT_TERMS: Optional[Union[List[str],str]] = ""
+    XCC_ACAD_PROGRAM_REQMTS: Optional[Union[List[str],str]] = ""
 
 
 class PyColleagueModel(BaseModel):
@@ -129,13 +172,14 @@ class RModel(BaseModel):
     scripts_path: Optional[str] = ""
 
 
-class ConfigModel(BaseSettings):
-    location: Optional[str] = Field(env="HAYWOODCC_CFG_FULL_PATH")
-    location_fn: Optional[str] = Field(env="HAYWOODCC_CFG_FN")
-    location_path: Optional[str] = Field(env="HAYWOODCC_CFG_PATH")
+#class ConfigModel(BaseSettings):
+class CCDWConfigModel(BaseModel):
+    location: Optional[str] = Field(validation_alias="CCDW_CFG_FULL_PATH")
+    location_fn: Optional[str] = Field(validation_alias="CCDW_CFG_FN")
+    location_path: Optional[str] = Field(validation_alias="CCDW_CFG_PATH")
 
 
-class Settings(BaseSettings):
+class Settings(BaseSettings, case_sensitive = False):
     school: Optional[SchoolModel] = SchoolModel()
     sql: Optional[SQLModel] = SQLModel()
     informer: Optional[Informer] = Informer()
@@ -144,33 +188,29 @@ class Settings(BaseSettings):
     pycolleague: Optional[PyColleagueModel] = PyColleagueModel()
     datamart: Optional[DatamartModel] = DatamartModel()
     R: Optional[RModel] = RModel()
-    config: Optional[ConfigModel] = ConfigModel()
+    #config: Optional[CCDWConfigModel] = CCDWConfigModel()
 
-    class Config:
-        env_file: str = ".env"
-        case_sensitive: bool = False
-        arbitrary_types_allowed: bool = True
-        validate_all: bool = False
-        extra: str = "allow"
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        arbitrary_types_allowed = True,
+        validate_default = False,
+        extra = "allow"
+    )
 
-        @classmethod
-        def customise_sources(
-            cls,
-            init_settings: SettingsSourceCallable,
-            env_settings: SettingsSourceCallable,
-            file_secret_settings: SettingsSourceCallable,
-        ) -> tuple[SettingsSourceCallable, ...]:
-            return (
-                init_settings,
-                env_settings,
-                yml_config_setting,
-            )
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return init_settings, env_settings, dotenv_settings, yml_config_setting(settings_cls)
 
-def get_config():
-    return Settings().dict()
-    
+
 if __name__ == "__main__":
-    testdict = Settings().dict()
+    testdict = Settings().model_dump()
 
     print(testdict)
     print("Done")
